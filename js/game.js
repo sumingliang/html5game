452e9a5fe372f4c53397c124225595d8d623c21b
sumingliang <sumingliang0920@gmail.com>
2015-09-19 17:24:56 +0800

    游戏界面

--- js/game.js
+++ js/game.js
@@ -9,5 +9,48 @@ var game = {
         loader.init();
         $('.gamelayer').hide();
         $('#gamestartscreen').show();
+
+        game.backgroundCanvas = document.getElementById('gamebackgroundcanvas');
+        game.backgroundContext = game.backgroundCanvas.getContext('2d');
+
+        game.foregroundCanvas = document.getElementById('gameforegroundcanvas');
+        game.foregroundContext = game.foregroundCanvas.getContext('2d');
+
+
+        game.canvasWidth = game.backgroundCanvas.width;
+        game.canvasHeight = game.foregroundCanvas.height;
+
+    },
+    start:function() {
+        // body...
+        $('.gamelayer').hide();
+        $('#gameinterfacescreen').show();
+        game.running = true;
+        game.refreshBackground = true;
+        game.drawingLoop();
+
+    },
+    gridSize:20,
+    backgroundChanged:true,
+
+    animationTimeout:100,
+    offsetX:0,
+    offsetY:0,
+    animationLoop:function()
+    {
+
+    },
+    drawingLoop:function()
+    {
+        if(game.refreshBackground)
+        {
+            game.backgroundContext.drawImage(game.currentMapImage,game.offsetX,game.offsetY,game.canvasWidth,
+            game.canvasHeight,0,0,game.canvasWidth,game.canvasHeight);
+            game.refreshBackground = false;
+        }
+        if (game.running)
+        {
+            requestAnimationFrame(game.drawingLoop);
+        }
     }
 }
--- js/singleplayer.js
+++ js/singleplayer.js
@@ -21,6 +21,9 @@ var singleplayer = {
         game.currentMapImage = loader.loadImage(level.mapImage);
         game.currentLevel = level;
 
+        game.offsetX = level.startX * game.gridSize;
+        game.offsetY = level.startY * game.gridSize;
+
         if (loader.loaded)
         {
             $("#entermission").removeAttr("disabled");
@@ -35,7 +38,13 @@ var singleplayer = {
         }
 
 	    // Load the mission screen with the current briefing
-	    $('#missonbriefing').html(level.briefing.replace('\n','<br><br>'));    
-	    $("#missionscreen").show();        
+	    $('#missonbriefing').html(level.briefing.replace('\n','<br><br>'));
+	    $("#missionscreen").show();
     },
+    play:function()
+    {
+        game.animationLoop();
+        game.animationInterval = setInterval(game.animationLoop,game.animationTimeout);
+        game.start();
+    }
 };
